dd54b10b6cca5c66694b2c7eb9ef2fff8ac33db3
Travis Chenn <TravisChen@TravisdeMacBook-Pro.local>
2019-11-09 09:35:08 +0800

    chore(git standard): 新增了Git提交风格检查工具,用于统一提交风格与构建 ChangeLog

--- src/components/index.js
+++ src/components/index.js
@@ -2,21 +2,21 @@ import TimePicker from '../components/Form/TimePicker'
 import ActionMenu from '../components/Action/ActionMenu'
 
 const components = {
-    TimePicker,
-    ActionMenu
+  TimePicker,
+  ActionMenu
 }
 
-const install = function(Vue) {
+const install = function (Vue) {
   if (install.installed) {
     return
   }
   Object.keys(components).forEach(key => {
-    Vue.component(components[key].name, components[key]);
+    Vue.component(components[key].name, components[key])
   })
 }
 
 if (typeof window !== 'undefined' && window.Vue) {
-  install(window.Vue);
+  install(window.Vue)
 }
 
 export default {
---
+++ src/standard.md
@@ -0,0 +1,5 @@
+Git 约定式提交规范
+http://www.ruanyifeng.com/blog/2016/01/commit_message_change_log.html
+
+语义化版本规范
+https://semver.org/lang/zh-CN/
