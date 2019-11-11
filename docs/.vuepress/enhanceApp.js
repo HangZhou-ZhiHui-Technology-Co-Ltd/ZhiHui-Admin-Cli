1c29f91ba357f1fe7435f53d5cb650b513142439
dingater <dingater@outlook.com>
2019-11-11 13:23:55 +0800

    feat(actionmenu.md): 添加动作菜单

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -2,8 +2,10 @@ import AntD from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css'
 
 import ZhAdvice from '../../src/components/advice'
+import ActionMenu from '../../src/components/actionmenu'
 
 export default ({ Vue }) => {
   Vue.use(AntD)
   Vue.use(ZhAdvice)
+  Vue.use(ActionMenu)
 }
--- src/components/advice/Component.vue
+++ src/components/advice/Component.vue
@@ -2,66 +2,66 @@
 import { isString } from 'lodash'
 import SELECT from 'ant-design-vue/es/select'
 export default {
-	name: 'zh-advice',
-	props: Object.assign({}, SELECT.props, {
-		local: {
-			type: Array,
-			default: () => {
-				return []
-			}
-		}
-	}),
-	data() {
-		return {
-			data: 'Vue',
-			advices: []
-		}
-	},
-	methods: {
-		/**
+  name: 'zh-advice',
+  props: Object.assign({}, SELECT.props, {
+    local: {
+      type: Array,
+      default: () => {
+        return []
+      }
+    }
+  }),
+  data () {
+    return {
+      data: 'Vue',
+      advices: []
+    }
+  },
+  methods: {
+    /**
 		 * 重载
 		 */
-		reload() {
-			const { local } = this
-			if (local) {
-				this.advices = local
-			}
-		},
-		/**
+    reload () {
+      const { local } = this
+      if (local) {
+        this.advices = local
+      }
+    },
+    /**
 		 * 转换
 		 * ---
 		 * @param { String | Object } origin 原始数据
 		 * ---
 		 */
-		_format(origin) {
-			if (isString(origin)) {
-				return { label: origin, value: origin }
-			}
-		},
-		/**
+    _format (origin) {
+      if (isString(origin)) {
+        return { label: origin, value: origin }
+      }
+    },
+    /**
 		 * 渲染 - 选项
 		 */
-		_render_option() {
-			const { local } = this
-			return this.advices.map(origin => {
-				const { label, value } = this._format(origin)
-				return <a-select-option value={value}>{label}</a-select-option>
-			})
-		},
-		/**
+    _render_option () {
+      const { local } = this
+      return this.advices.map(origin => {
+        const { label, value } = this._format(origin)
+        return <a-select-option value={value}>{label}</a-select-option>
+      })
+    },
+    /**
 		 * 配置 - 选择器
 		 */
-		_vnode_selectt() {
-			return {
-				props: Object.assign({}, this.$props)
-			}
-		}
-	},
-	mounted() {
-		this.reload()
-	},
-	render() {
-		return <a-select>{this._render_option()}</a-select>
-	}
+    _vnode_selectt () {
+      return {
+        props: Object.assign({}, this.$props)
+      }
+    }
+  },
+  mounted () {
+    this.reload()
+  },
+  render () {
+    return <a-select>{this._render_option()}</a-select>
+  }
 }
 </script>
