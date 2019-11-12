6bb72ed6d932d42bd78495072cf0de1ca491049f
TravisChen <travischenn@outlook.com>
2019-11-12 09:36:52 +0800

    docs(工作计划): 添加 2019-11-12 时段一的工作计划

---
+++ src/components/advice/demo/index.vue
@@ -0,0 +1,16 @@
+<script>
+import asd from './test.md'
+export default {
+	name: 'hello-world',
+	render() {
+		return (
+			<div>
+				<span>asdasdsa</span>
+				<div>
+					<asd />
+				</div>
+			</div>
+		)
+	}
+}
+</script>
