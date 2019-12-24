8a21cc0802b76aa74cbd2205dbf1bdb72f2a629b
dingater <dingater@outlook.com>
2019-12-24 17:44:20 +0800

    feat(组件目录): 罗列业务、布局和普通三个模块的组件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -44,6 +44,14 @@ module.exports = {
      */
     nav: [
       {
+        text: '混入',
+        link: '/mixins/'
+      },
+      {
+        text: '插件',
+        link: '/plugins/'
+      },
+      {
         text: '组件',
         link: '/components/'
       },
@@ -65,22 +73,282 @@ module.exports = {
      * ---
      */
     sidebar: {
+      '/mixins/': [
+        {
+          title: '混入',
+          collapsable: false,
+          children: [
+            {
+              title: 'tudo',
+              path: '../mixins/todo'
+            }
+          ]
+        }
+      ],
+      '/plugins/': [
+        {
+          title: '插件',
+          collapsable: false,
+          children: [
+            {
+              title: 'tudo',
+              path: '../plugins/todo'
+            }
+          ]
+        }
+      ],
       '/components/': [
         {
-          title: '组件',
+          title: '普通',
           collapsable: false,
           children: [
             {
               title: 'Advice 建议',
-              path: '../components/advice'
+              path: '../components/Common/advice'
             },
             {
               title: 'ActionMenu 动作菜单',
-              path: '../components/actionmenu'
+              path: '../components/Common/actionmenu'
             },
             {
               title: 'IconFont 自定义图标',
-              path: '../components/iconfont'
+              path: '../components/Common/iconfont'
+            },
+            {
+              title: 'PageTable 分页表格',
+              path: '../components/Common/pagetable'
+            },
+            {
+              title: 'PageHeaderPanel 页头面板',
+              path: '../components/Common/PageHeaderPanel'
+            },
+            {
+              title: 'TimePicker 时间选择器',
+              path: '../components/Common/timepicker'
+            },
+            {
+              title: 'Wangeditor 富文本编辑器',
+              path: '../components/Common/Wangeditor'
+            },
+            {
+              title: 'ZHCommitBar 提交条',
+              path: '../components/Common/ZHCommitBar'
+            },
+            {
+              title: 'ZHDescriptions 复杂表格',
+              path: '../components/Common/ZHDescriptions'
+            },
+            {
+              title: 'ZHGenderTag 性别标签',
+              path: '../components/Common/ZHGenderTag'
+            },
+            {
+              title: 'ZHPageHeader 页头',
+              path: '../components/Common/ZHPageHeader'
+            },
+            {
+              title: 'ZHResult 结果窗',
+              path: '../components/Common/ZHResult'
+            },
+            {
+              title: 'ZHResultModal 审核结果窗',
+              path: '../components/Common/ZHResultModal'
+            },
+            {
+              title: 'ZHTree 树选择器',
+              path: '../components/Common/ZHTree'
+            }
+          ]
+        },
+        {
+          title: '业务',
+          collapsable: false,
+          children: [
+            {
+              title: 'ReadCard 公告卡片',
+              path: '../components/Service/ReadCard'
+            },
+            {
+              title: 'ReadList 已读清单',
+              path: '../components/Service/ReadList'
+            },
+            {
+              title: 'ReadModal 公告弹出窗',
+              path: '../components/Service/ReadModal'
+            },
+            {
+              title: 'FollowModal 沟通记录',
+              path: '../components/Service/FollowModal'
+            },
+            {
+              title: 'AnnexTable 设备附件列表',
+              path: '../components/Service/AnnexTable'
+            },
+            {
+              title: 'AccurateNote 设备校准记录',
+              path: '../components/Service/AccurateNote'
+            },
+            {
+              title: 'MaintainNote 维护保养记录',
+              path: '../components/Service/MaintainNote'
+            },
+            {
+              title: 'ObservedNote 质量监控记录',
+              path: '../components/Service/ObservedNote'
+            },
+            {
+              title: 'SurpriseNote 期间核查记录',
+              path: '../components/Service/SurpriseNote'
+            },
+            {
+              title: 'NoteTable 记录表格',
+              path: '../components/Service/ReadCard'
+            },
+            {
+              title: 'AccuratePlan 设备校准计划',
+              path: '../components/Service/AccuratePlan'
+            },
+            {
+              title: 'FeaturesPlan 设备校准需求',
+              path: '../components/Service/FeaturesPlan'
+            },
+            {
+              title: 'MaintainPlan 设备保养计划',
+              path: '../components/Service/MaintainPlan'
+            },
+            {
+              title: 'ObservedPlan 设备监控计划',
+              path: '../components/Service/ObservedPlan'
+            },
+            {
+              title: 'SurprisePlan 设备核查计划',
+              path: '../components/Service/SurprisePlan'
+            },
+            {
+              title: 'PlanTable 计划表格',
+              path: '../components/Service/PlanTable'
+            },
+            {
+              title: 'SupplyAbility 供应商内容',
+              path: '../components/Service/SupplyAbility'
+            },
+            {
+              title: 'SupplyCertify 供应商资质证明',
+              path: '../components/Service/SupplyCertify'
+            },
+            {
+              title: 'DraftDrawer 草稿箱',
+              path: '../components/Service/DraftDrawer'
+            },
+            {
+              title: 'CalibrationSample 校准委托单样品',
+              path: '../components/Service/CalibrationSample'
+            },
+            {
+              title: 'DetectionSample 检测委托单样品',
+              path: '../components/Service/DetectionSample'
+            },
+            {
+              title: 'MissionInfoCard 委托单项目详情',
+              path: '../components/Service/MissionInfoCard'
+            },
+            {
+              title: 'MissionTplModal 委托单原始记录弹窗',
+              path: '../components/Service/MissionTplModal'
+            },
+            {
+              title: 'PapersModal 导出报告单弹窗',
+              path: '../components/Service/PapersModal'
+            },
+            {
+              title: 'ReportNumber 报告编号',
+              path: '../components/Service/ReportNumber'
+            },
+            {
+              title: 'SampleModal 新增流转记录弹出窗',
+              path: '../components/Service/SampleModal'
+            },
+            {
+              title: 'FileDownload 文件下载',
+              path: '../components/Service/FileDownload'
+            },
+            {
+              title: 'FileListModal 文件列表弹窗',
+              path: '../components/Service/FileListModal'
+            },
+            {
+              title: 'FileModal 文件弹窗',
+              path: '../components/Service/FileModal'
+            },
+            {
+              title: 'FilePicker 文件选择器',
+              path: '../components/Service/FilePicker'
+            },
+            {
+              title: 'FileTable 文件列表',
+              path: '../components/Service/FileTable'
+            },
+            {
+              title: 'FileUpload 文件上传',
+              path: '../components/Service/FileUpload'
+            },
+            {
+              title: 'FinanceInvoiceModal 财务管理开具发票弹窗',
+              path: '../components/Service/FinanceInvoiceModal'
+            },
+            {
+              title: 'FinanceInvoiceTable 财务管理审核页发票表格',
+              path: '../components/Service/FinanceInvoiceTable'
+            },
+            {
+              title: 'GenderAvatar 性别头像',
+              path: '../components/Service/GenderAvatar'
+            },
+            {
+              title: 'PlanChecker 计划检查器',
+              path: '../components/Service/PlanChecker'
+            },
+            {
+              title: 'ApplyListTable 采购申请清单',
+              path: '../components/Service/ApplyListTable'
+            },
+            {
+              title: 'ReplyListTable 领料申请清单',
+              path: '../components/Service/ReplyListTable'
+            },
+            {
+              title: 'StockListTable 库存清单',
+              path: '../components/Service/StockListTable'
+            },
+            {
+              title: 'ReviewFile 审核文件列表',
+              path: '../components/Service/ReviewFile'
+            },
+            {
+              title: 'ReviewResultModal 审核结果弹窗',
+              path: '../components/Service/ResultModal'
+            },
+            {
+              title: 'ReviewFileModal 审核文件弹窗',
+              path: '../components/Service/ReviewFileModal'
+            },
+            {
+              title: 'WorkerNoteModal 计划记录清单弹窗',
+              path: '../components/Service/ReviewItem'
+            },
+            {
+              title: 'WorkerPlan 计划清单详情',
+              path: '../components/Service/ReviewFile'
+            }
+          ]
+        },
+        {
+          title: '布局',
+          collapsable: false,
+          children: [
+            {
+              title: 'BaseLayout 基础布局',
+              path: '../components/Layout/BaseLayout'
             }
           ]
         }
